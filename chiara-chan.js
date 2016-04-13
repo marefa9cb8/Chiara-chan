d3866bea89598757aa3d6be124ffe8cd06e32ec1
balar <marefa9cb8@gmail.com>
2016-04-14 03:46:49 +0900

    make a tweet -> slack

--- chiara-chan.js
+++ chiara-chan.js
@@ -1,13 +1,21 @@
 var Promise = require('bluebird');
 var slack = require('slack');
+var twitter = require('twitter');
 var config = require('./config.json');
+
+var bot = new twitter({
+  consumer_key  :config.TwitterConsumerKey,
+  consumer_secret :config.TwitterConsumerSecret,
+  access_token_key  :config.TwitterAccessToken,
+  access_token_secret :config.TwitterAccessTokenSecret
+});
 
 function slackPost(str){
   return new Promise(function(resolve,reject){
     var param = {
-      token:config.SlackBotToken,
-      channel:config.SlackChannelID,
-      text:str
+      token :config.SlackBotToken,
+      channel :config.SlackChannelID,
+      text  :str
     };
     slack.chat.postMessage(param,function(err,data){
       if(err) reject(err);
@@ -15,4 +23,26 @@ function slackPost(str){
     });
   });
 }
+
+function getTweetURL(screen_name, id){
+  return 'https://twitter.com/' + screen_name + '/status/' + id;
+}
+
+bot.stream('user', function(stream){
+  stream.on('favorite',function(data){
+    if(data.event === 'favorite' 
+    && data.source.screen_name === config.TwitterScreenName
+    && data.target_object.extended_entities
+    ){
+      var url = getTweetURL(data.target.screen_name, data.target_object.id_str);
+      slackPost(url);
+      console.log(data.target_object.extended_entities);
+    }
+  });
+  stream.on('error', function(error){
+    var errStr = error;
+    slackPost("error");
+  });
+});
+
 
